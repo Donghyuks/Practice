99c076aa272fa613b228f2ef8382c2435ba84aeb
DongHyuk <ciw0504@naver.com>
2022-01-11 10:08:55 +0900

    Delete Dummy and Update Port
    1. 필요없는 파일 삭제
    2. 게임서버 포트 5개 추가.

--- EaterGameServer/ImportLibrary/Inc/GameServerLobbyPacketDefine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/*
-	2021/12/20 10:54 - CDH
-
-	< 변경사항 >
-		1. GameServer(Client), Lobby(Server) 간의 패킷 정의.
-
-*/
--- EaterTestClient/ImportLibrary/Inc/PortIPDefine.h
+++ EaterTestClient/ImportLibrary/Inc/PortIPDefine.h
@@ -7,7 +7,14 @@
 
 // 공식 Port
 #define DATABASE_PORT		3306
+
 // 공식 Port 번호가 없는 7천번대로..
 #define LOGIN_SERVER_PORT	7000
-#define GAME_SERVER_PORT	7001
-#define LOBBY_SERVER_PORT	7002
+#define LOBBY_SERVER_PORT	7001
+
+// 분산서버로 관리할 게임서버 최대 5개.
+#define GAME_SERVER_PORT_1	7002
+#define GAME_SERVER_PORT_2	7003
+#define GAME_SERVER_PORT_3	7004
+#define GAME_SERVER_PORT_4	7005
+#define GAME_SERVER_PORT_5	7006
